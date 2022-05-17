3bafc60b07d755a13fb18c63798b438721252832
joshua <joshua.ibrahim@panderasystems.com>
2022-05-17 14:28:14 -0400

    added some missing project ids

--- main.tf
+++ main.tf
@@ -29,30 +29,25 @@ module "bigquery" {
 
 module "pubsub" {
   source = "./modules/pubsub"
-  project_id = var.project_id
   env = var.env
 }
 
 module "cloud_storage" {
   source = "./modules/cloudstorage"
-  project_id = var.project_id
   env = var.env
 }
 
 module "cloud_functions" {
   source = "./modules/cloudfunctions"
-  project_id = var.project_id
   env = var.env
 }
 
 module "cloud_sql" {
   source = "./modules/cloudsql"
-  project_id = var.project_id
   env = var.env
 }
 
 module "firebase" {
   source = "./modules/cloudsql"
-  project_id = var.project_id
   env = var.env
 }
