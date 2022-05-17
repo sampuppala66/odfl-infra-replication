961ced488f2af57772c99909743e943571d46e60
joshua <joshua.ibrahim@panderasystems.com>
2022-05-17 14:00:41 -0400

    added some missing project ids

--- modules/cloudfunctions/main.tf
+++ modules/cloudfunctions/main.tf
@@ -7,6 +7,7 @@ resource "google_storage_bucket_object" "drone_archive" {
   name   = var.drone_archive_name
   bucket = google_storage_bucket.drone_cloud_function_bucket.name
   source = var.drone_archive_source
+  project = google_project.project_drone.project_id
 }
 
 
@@ -14,6 +15,7 @@ resource "google_cloudfunctions_function" "drone_function" {
   name        = var.drone_function_name
   description = var.cloud_function_description
   runtime     = var.cloud_function_runtime
+  project = google_project.project_drone.project_id
 
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.drone_cloud_function_bucket.name
@@ -21,11 +23,4 @@ resource "google_cloudfunctions_function" "drone_function" {
   trigger_http          = true
   timeout               = 60
   entry_point           = var.cloud_function_entry_point
-#   labels = {
-#     my-label = "my-label-value"
-#   }
-
-#   environment_variables = {
-#     MY_ENV_VAR = "my-env-var-value"
-#   }
 }
