fd37625b9dbac6f99fad7423cc1a00c28bcdb8f0
joshua <joshua.ibrahim@panderasystems.com>
2022-05-17 13:32:35 -0400

    added project id to cloud storage

--- modules/cloudstorage/main.tf
+++ modules/cloudstorage/main.tf
@@ -3,9 +3,6 @@ resource "google_storage_bucket" "drone_storage" {
   name          = var.drone_storage_name
   location      = var.drone_storage_location
   force_destroy = true
-<<<<<<< HEAD
   project = google_project.project_drone.project_id
-=======
 
->>>>>>> f5fbd93a4c849ec521cd239e6f483a7116f9af0f
 }
