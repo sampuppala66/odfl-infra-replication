77b84842a7627334ca1ee91e8f6f7d6139cca330
joshua <joshua.ibrahim@panderasystems.com>
2022-05-19 13:11:26 -0400

    creating dependencies

--- modules/bigquery/main.tf
+++ modules/bigquery/main.tf
@@ -1,8 +1,12 @@
+module "project"{
+    source = "../project"
+}
+
 resource "google_bigquery_dataset" "drone_data" {
   dataset_id                  = var.drone_dataset_id
   friendly_name                = var.drone_dataset_name
   description                 = var.dataset_description
-  project                     = var.project_id
+  project                     = module.project.drone_project_id
   location                    = var.location
   labels    = var.dataset_labels
 }
@@ -11,7 +15,7 @@ resource "google_bigquery_dataset" "audit_data" {
   dataset_id                  = var.audit_dataset_id
   friendly_name                = var.audit_dataset_name
   description                 = var.audit_dataset_description
-  project                     = var.audit_project_id
+  project                     = module.project.audit_project_id
   location                    = var.location
   labels    = var.dataset_labels
 }
---
+++ modules/project/outputs.tf
@@ -0,0 +1,7 @@
+output drone_project_id {
+    value = google_project.project_drone.project_id
+}
+
+output audit_project_id {
+    value = google_project.project_audit.project_id
+}
