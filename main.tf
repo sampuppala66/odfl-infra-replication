00b3dfb4841fa5b62bf50492b155af5d94e1bb78
joshua <joshua.ibrahim@panderasystems.com>
2022-05-19 14:36:34 -0400

    fixes

--- main.tf
+++ main.tf
@@ -4,54 +4,54 @@ module "folders" {
 
 module "project" {
   source = "./modules/project"
-  project_id = "${var.project_id}-${var.env}-12345"
+  project_id = "${var.project_id}-${var.env}"
   env = var.env
 }
 
 module "networks" {
   source = "./modules/networks"
-  project_id = "${var.project_id}-${var.env}-12345"
+  project_id = "${var.project_id}-${var.env}"
   env = var.env
 }
 
 module "iam" {
   source = "./modules/iam"
-  project_id = "${var.project_id}-${var.env}-12345"
+  project_id = "${var.project_id}-${var.env}"
   env = var.env
 }
 
 module "bigquery" {
   source = "./modules/bigquery"
-  project_id = "${var.project_id}-${var.env}-12345"
+  project_id = "${var.project_id}-${var.env}"
   env = var.env
 }
 
 module "pubsub" {
   source = "./modules/pubsub"
-  project_id = "${var.project_id}-${var.env}-12345"
+  project_id = "${var.project_id}-${var.env}"
   env = var.env
 }
 
 module "cloudstorage" {
   source = "./modules/cloudstorage"
-  project_id = "${var.project_id}-${var.env}-12345"
+  project_id = "${var.project_id}-${var.env}"
   env = var.env
 }
 
 # module "cloud_functions" {
 #   source = "./modules/cloudfunctions"
-#   project_id = "${var.project_id}-${var.env}-12345"
+#   project_id = "${var.project_id}-${var.env}"
 #   env = var.env
 # }
 
 module "cloudsql" {
   source = "./modules/cloudsql"
-  project_id = "${var.project_id}-${var.env}-12345"
+  project_id = "${var.project_id}-${var.env}"
   env = var.env
 }
 
 module "firebase" {
   source = "./modules/cloudsql"
-  project_id = "${var.project_id}-${var.env}-12345"
+  project_id = "${var.project_id}-${var.env}"
   env = var.env
 }
