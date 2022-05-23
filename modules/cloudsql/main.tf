ea2c33ec0b19827ea07e92b733f8a36a0c34b8ce
joshua <joshua.ibrahim@panderasystems.com>
2022-05-23 19:27:03 -0400

    fixes

--- modules/cloudsql/main.tf
+++ modules/cloudsql/main.tf
@@ -1,5 +1,5 @@
 resource "google_sql_database_instance" "drone_instance" {
-  name             = "${var.project_id}-${var.drone_database_name}"
+  name             = "${var.project_id}-${random_string.four_chars.result}"
   region           = var.drone_database_region
   database_version = var.drone_database_version
   settings {
@@ -9,8 +9,15 @@ resource "google_sql_database_instance" "drone_instance" {
   deletion_protection  = var.drone_database_protection
 }
 
-# resource "google_sql_database" "drone_cloud_sql" {
-#   name     = "${var.project_id}-${var.cloud_sql_name}"
-#   instance = google_sql_database_instance.drone_instance.name
-#   project = "${var.project_id}"
-# }
+resource "google_sql_database" "drone_cloud_sql" {
+  name     = "${var.project_id}-${var.cloud_sql_name}"
+  instance = google_sql_database_instance.drone_instance.name
+  project = "${var.project_id}"
+}
+
+
+resource "random_string" "four_chars" {
+  length  = 4
+  upper   = false # instance names can only have lowercase letters, numbers, and hyphens
+  special = false
+}
