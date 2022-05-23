6a83f0e195189a1eda3c33c60db6386af934c48d
joshua <joshua.ibrahim@panderasystems.com>
2022-05-23 17:02:50 -0400

    fixes

--- modules/cloudsql/main.tf
+++ modules/cloudsql/main.tf
@@ -9,8 +9,8 @@ resource "google_sql_database_instance" "drone_instance" {
   deletion_protection  = var.drone_database_protection
 }
 
-resource "google_sql_database" "drone_cloud_sql" {
-  name     = "${var.project_id}-${var.cloud_sql_name}"
-  instance = google_sql_database_instance.drone_instance.name
-  project = "${var.project_id}"
-}
+# resource "google_sql_database" "drone_cloud_sql" {
+#   name     = "${var.project_id}-${var.cloud_sql_name}"
+#   instance = google_sql_database_instance.drone_instance.name
+#   project = "${var.project_id}"
+# }
