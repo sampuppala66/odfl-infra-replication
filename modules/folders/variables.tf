a3ac5ff37377e0f7e53fc4c8711f42327c7a9c67
joshua <joshua.ibrahim@panderasystems.com>
2022-05-24 14:23:22 -0400

    fixes

--- modules/folders/variables.tf
+++ modules/folders/variables.tf
@@ -1,19 +1,19 @@
 variable "gov_folder_name" {
     description = "Gov folder name"
     type = string
-    default = "sba-gov-1"
+    default = "sba-gov"
 }
 variable "drone_folder_name" {
     description = "Drone folder name"
     type = string
-    default = "sba-drone-1"
+    default = "sba-drone"
 }
 
 
 variable "shared_folder_name" {
     description = "Shared folder name"
     type = string
-    default = "sba-shared-1"
+    default = "sba-shared"
 }
 variable "organization_path" {
     description = "Organization Path"
